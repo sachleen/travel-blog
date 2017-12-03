546a3003296cbadfbbe1414b9fe5da0c3098a26c
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-12-03 11:42:33 -0800

    trying a new deploy script that checks secret key

--- gitpull.php
+++ gitpull.php
@@ -1,22 +1,57 @@
 <?php
-$agent = $_SERVER['HTTP_USER_AGENT'];
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via github securely, more deets here:
+     *
+     *      https://gist.github.com/limzykenneth/baef1b190c68970d50e1
+     */
 
-$secretFile = __DIR__ . '/../../.gitpullsecretkey';
-$fh = fopen($secretFile, 'r');
-$secretKey = fread($fh, filesize($secretFile));
-fclose($fh);
+    // The header information which will be verified
+    $agent=$_SERVER['HTTP_USER_AGENT'];
+    $signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+    $body=@file_get_contents('php://input');
 
-$commands = array(
-	'git pull origin master'
-);
+    // The commands
+    $commands = array(
+        'git pull origin master'
+    );
 
-if (strpos($agent,'GitHub-Hookshot') !== false) {
-	foreach($commands AS $command) {
-		$tmp = shell_exec($command);
+    base64_encode($agent);
+    base64_encode($signature);
+
+    if (strpos($agent,'GitHub-Hookshot') !== false){
+        if (hash_equals($signature, verify_request())){
+            // Run the commands
+            foreach($commands AS $command){
+                // Run it
+                $tmp = shell_exec($command);
+            }
+            echo "Deploy successful.";
+        }else{
+            header('HTTP/1.1 403 Forbidden');
+            echo "Invalid request 1.";
+        }
+    }else{
+        header('HTTP/1.1 403 Forbidden');
+        echo "Invalid request 2.";
+    }
+
+    // Generate the hash verification with the request body and the key stored in your .htaccess file
+    function verify_request(){
+        $message = $GLOBALS['body'];
+        $key     = getSecretKey();
+		$hash    = hash_hmac("sha1", $message, $key);
+		$hash 	 = "sha1=".$hash;
+		return $hash;
+    } 
+	
+	function getSecretKey() {
+		$secretFile = __DIR__ . '/../../.gitpullsecretkey';
+		$fh = fopen($secretFile, 'r');
+		$secretKey = fread($fh, filesize($secretFile));
+		fclose($fh);
+		return $secretKey;
 	}
-	echo "complete";
-	echo $secretKey;
-} else {
-	header('HTTP/1.0 403 Forbidden');
-}
+
 ?>
