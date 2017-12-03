660444cf64df45ccd6f5d8515ee8398e3d47f9d8
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-12-03 11:32:16 -0800

    added very little security to git pull script.

--- gitpull.php
+++ gitpull.php
@@ -1,13 +1,16 @@
-<?php 
-// The header information which will be verified
-$agent=$_SERVER['HTTP_USER_AGENT'];
-$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
-$body=@file_get_contents('php://input');
+<?php
+$agent = $_SERVER['HTTP_USER_AGENT'];
 
-print_r($_SERVER);
-echo $agent;
-echo $signature;
-echo $body;
+$commands = array(
+	'git pull origin master'
+);
 
-// `git pull`
+if (strpos($agent,'GitHub-Hookshot') !== false) {
+	foreach($commands AS $command) {
+		$tmp = shell_exec($command);
+	}
+	echo "complete";
+} else {
+	header('HTTP/1.0 403 Forbidden');
+}
 ?>
