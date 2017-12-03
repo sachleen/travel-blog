99ffd8abd027e75036eedc77598f001f4b957ad3
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-12-03 12:05:44 -0800

    updated documentation on script. removed unneeded base64 lines.

--- gitpull.php
+++ gitpull.php
@@ -1,58 +1,56 @@
 <?php
-    /**
-     * GIT DEPLOYMENT SCRIPT
-     *
-     * Used for automatically deploying websites via github securely, more deets here:
-     *
-     *      https://gist.github.com/limzykenneth/baef1b190c68970d50e1
-     */
+/**
+ * GIT DEPLOYMENT SCRIPT
+ *
+ * Used for automatically deploying websites via github securely, more deets here:
+ *
+ *      https://gist.github.com/limzykenneth/baef1b190c68970d50e1
+ */
 
-    // The header information which will be verified
-    $agent=$_SERVER['HTTP_USER_AGENT'];
-    $signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
-    $body=@file_get_contents('php://input');
+// The header information which will be verified
+$agent=$_SERVER['HTTP_USER_AGENT'];
+$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+$body=@file_get_contents('php://input');
 
-    // The commands
-    $commands = array(
-        'git pull origin master'
-    );
+// The commands
+$commands = array(
+	'git pull origin master'
+);
 
-    base64_encode($agent);
-    base64_encode($signature);
+if (strpos($agent,'GitHub-Hookshot') !== false){
 
-    if (strpos($agent,'GitHub-Hookshot') !== false){
-
-        if (hash_equals($signature, verify_request())){
-            // Run the commands
-            foreach($commands AS $command){
-                // Run it
-                $tmp = shell_exec($command);
-            }
-            echo "Deploy successful.";
-        }else{
-            header('HTTP/1.1 403 Forbidden');
-            echo "Invalid request 1.";
-        }
-    }else{
-        header('HTTP/1.1 403 Forbidden');
-        echo "Invalid request 2.";
-    }
-
-    // Generate the hash verification with the request body and the key stored in your .htaccess file
-    function verify_request(){
-        $message = $GLOBALS['body'];
-        $key     = getSecretKey();
-		$hash    = hash_hmac("sha1", $message, $key);
-		$hash 	 = "sha1=".$hash;
-		return $hash;
-    } 
-	
-	function getSecretKey() {
-		$secretFile = __DIR__ . '/../../.gitpullsecretkey';
-		$fh = fopen($secretFile, 'r');
-		$secretKey = fread($fh, filesize($secretFile));
-		fclose($fh);
-		return trim($secretKey);
+	if (hash_equals($signature, verify_request())){
+		// Run the commands
+		foreach($commands AS $command){
+			// Run it
+			$tmp = shell_exec($command);
+		}
+		echo "Deploy successful.";
+	}else{
+		header('HTTP/1.1 403 Forbidden');
+		echo "Invalid request 1.";
 	}
+}else{
+	header('HTTP/1.1 403 Forbidden');
+	echo "Invalid request 2.";
+}
+
+// Generate the hash verification with the request body and the key stored in your .htaccess file
+function verify_request(){
+	$message = $GLOBALS['body'];
+	$key     = getSecretKey();
+	$hash    = hash_hmac("sha1", $message, $key);
+	$hash 	 = "sha1=".$hash;
+	return $hash;
+} 
+
+// Gets the secret key from a file outside public_html. The file should contain nothing but the key on one line only.
+function getSecretKey() {
+	$secretFile = __DIR__ . '/../../.gitpullsecretkey';
+	$fh = fopen($secretFile, 'r');
+	$secretKey = fread($fh, filesize($secretFile));
+	fclose($fh);
+	return trim($secretKey);
+}
 
 ?>
