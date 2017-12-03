9c6afa69026fa3781c64816aaf4afbe5e2230784
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-12-03 11:54:37 -0800

    more deubg

--- gitpull.php
+++ gitpull.php
@@ -21,8 +21,11 @@
     base64_encode($signature);
 
     if (strpos($agent,'GitHub-Hookshot') !== false){
+		echo $GLOBALS['body'];
+		echo "------------";
 		echo $signature . '\n\n';
 		echo verify_request();
+		
         if (hash_equals($signature, verify_request())){
             // Run the commands
             foreach($commands AS $command){
