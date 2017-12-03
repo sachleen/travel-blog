eba700a05495be2daeb8ff4e51d60efbeeaeb299
Sachleen Sandhu <sachleen.sandhu@gmail.com>
2017-12-03 11:00:43 -0800

    test gitpull response

--- gitpull.php
+++ gitpull.php
@@ -1,1 +1,13 @@
-<?php `git pull`
+<?php 
+// The header information which will be verified
+$agent=$_SERVER['HTTP_USER_AGENT'];
+$signature=$_SERVER['HTTP_X_HUB_SIGNATURE'];
+$body=@file_get_contents('php://input');
+
+print_r($_SERVER);
+echo $agent;
+echo $signature;
+echo $body;
+
+// `git pull`
+?>
